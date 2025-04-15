dc2e3e635f41154c8ef81a8ba72942f839cc75b8
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2025-04-15 19:47:25 +0300

    add config template

---
+++ postgres/providers/config.go
@@ -0,0 +1,31 @@
+package providers_postgres
+
+import "os"
+
+type Provider struct {
+}
+
+func Init() (*Provider, error) {
+	var err error
+	provider := &Provider{}
+
+	const connectionString = "postgres://postgres:postgres@localhost:5432/postgres"
+
+	// Initialize the driver
+	err := {{.ProjectName}}_driver.Initialize(connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return provider, nil
+}
+
+// Proxy function to the driver
+func StartTransaction() (*appy_driver.Tx, error) {
+	return appy_driver.StartTransaction()
+}
+
+// Proxy function to the driver
+func RunTransaction(fn appy_driver.TransactionFn) error {
+	return appy_driver.RunTransaction(fn)
+}
