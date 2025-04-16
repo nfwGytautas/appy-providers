315e45dc8188cd109e213fb66afd7c00cc79b723
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2025-04-16 21:27:26 +0300

    write or create cache file

--- postgres/tools/cache.py
+++ postgres/tools/cache.py
@@ -53,7 +53,7 @@ class QueryCache:
     def save(self) -> None:
         pathlib.Path(self.__cache_file).parent.mkdir(parents=True, exist_ok=True)
 
-        with open(self.__cache_file, "w", encoding="utf-8") as f:
+        with open(self.__cache_file, "w+", encoding="utf-8") as f:
             f.write(json.dumps(self.__cache))
 
     def get_outdated(self) -> list[str]:
