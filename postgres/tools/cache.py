d851ba4f3bb4ee60fa2d0751fea4d7c5a9ce63db
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2025-04-16 21:26:25 +0300

    make cache dir if not exists

--- postgres/tools/cache.py
+++ postgres/tools/cache.py
@@ -51,6 +51,8 @@ class QueryCache:
             self.__cache[file]["deleted"] = True
 
     def save(self) -> None:
+        pathlib.Path(self.__cache_file).parent.mkdir(parents=True, exist_ok=True)
+
         with open(self.__cache_file, "w", encoding="utf-8") as f:
             f.write(json.dumps(self.__cache))
 
